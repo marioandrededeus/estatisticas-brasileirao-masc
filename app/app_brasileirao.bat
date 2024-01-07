9d12985060db15edb3f4271248acf2471b21ff9c
Mario-Deus_sgo <Mario.Deus@saint-gobain.com>
2024-01-07 13:25:42 -0300

    Updating the directory path in the .bat file

--- app/app_brasileirao.bat
+++ app/app_brasileirao.bat
@@ -1,5 +1,5 @@
 set root=C:\ProgramData\Anaconda3
 call %root%\Scripts\activate.bat
 call activate pycaret3
-call streamlit run "C:\Users\m4005001\Documents\_SG\Pessoal\Brasileirao\app\app.py" --theme.base="light"
+call streamlit run "C:\Users\m4005001\Documents\_SG\Pessoal\Estatisticas_Brasileirao\estatisticas-brasileirao-masc\app\app.py" --theme.base="light"
 PAUSE
